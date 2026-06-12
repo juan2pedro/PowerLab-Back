0759558d112c6483afb87e35f1b493eaf0be17a1
Juan Pedro Mancera Taboada <juanpemt@gmail.com>
2026-06-12 17:38:16 +0200

    fix and no use

--- src/main/java/com/jpmt/powerlab/models/mappers/TrainingSessionTemplateMapper.java
+++ src/main/java/com/jpmt/powerlab/models/mappers/TrainingSessionTemplateMapper.java
@@ -17,7 +17,7 @@ public interface TrainingSessionTemplateMapper {
     TrainingSessionTemplateSummaryResponse toSummaryResponse(TrainingSessionTemplate entity);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "templates", ignore = true)
+    @Mapping(target = "setTemplates", ignore = true)
     TrainingSessionTemplate toEntity(TrainingSessionTemplateRequest dto);
 
     @Mapping(target = "id", ignore = true)
